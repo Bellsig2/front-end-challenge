c428fa78cf962660d5a3850a20a2350ac20c52f0
Bellsig2 <75826074+Bellsig2@users.noreply.github.com>
2023-05-18 02:20:42 +0900

    .
    .

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -1,15 +1,38 @@
 import style from "./App.module.scss";
 import classnames from "classnames/bind";
+
 const cn = classnames.bind(style);
 
 function App() {
   return (
     <div className={cn("App")}>
       <Nav></Nav>
-    <div className={cn("flex")}>
+      <div className={cn("flex")}>
         <SideBar></SideBar>
         <View></View>
+      </div>
     </div>
+  );
+}
+
+function ChatBox() {
+  return (
+    <div className={cn("chatboxWrapper")}>
+      <div className={cn("chatbox")}>
+        <div className={cn("avatar")}>
+          <img src="https://ca.slack-edge.com/T05783L0872-U056FE737U5-2683da3d60f6-48"></img>
+        </div>
+        <div className={cn("")}>
+          <div className={cn("userWrapper")}>
+            <div className={cn("userName")}>강동욱욱자</div>
+            <span className={cn("timeStamp")}>오후 3:39</span>
+          </div>
+          <span className={cn("contents")}>
+            이것은 나머지 모든 것을 위한 채널입니다. 팀원들이 농담하거나
+            순간적인 아이디어나 재미있는 GIF를 공유하는 곳이죠! 마음껏 즐기세요!
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
@@ -18,6 +41,7 @@ function View() {
   return (
     <div className={cn("view")}>
       <div className={cn("header")}>#랜덤</div>
+      <ChatBox></ChatBox>
     </div>
   );
 }
@@ -43,9 +67,20 @@ function SideBar() {
           <div className={cn("icon-message")}></div>
         </div>
         <div className={cn("list")}>
-          <ul>
-            <li># 랜덤</li>
-          </ul>
+          <div className={cn("wrapper")}>
+            <div className={cn("channelName", "item")}>
+              <span className={cn("icon")}>#</span>
+              <span className={cn("")}>랜덤</span>
+            </div>
+            <div className={cn("channelName", "item")}>
+              <span className={cn("icon")}>#</span>
+              <span className={cn("")}>랜덤</span>
+            </div>
+            <div className={cn("channelName", "item")}>
+              <span className={cn("icon")}>#</span>
+              <span className={cn("")}>랜덤</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
