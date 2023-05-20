885641579d35bd3a6ff25f144f56e12371a46f9f
Bellsig2 <75826074+Bellsig2@users.noreply.github.com>
2023-05-20 14:55:57 +0900

    use channel props and use react-router-dom
    근데 오류를 추가한

--- slack-clone/src/components/sidebar/SideBar.jsx
+++ slack-clone/src/components/sidebar/SideBar.jsx
@@ -1,9 +1,11 @@
-import style from "./App.module.scss";
+import style from "./SideBar.module.scss";
 import classnames from "classnames/bind";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const cn = classnames.bind(style);
 
-export default function SideBar() {
+export default function SideBar({ channels }) {
   return (
     <div className={cn("sidebar")}>
       <div className={cn("workSpace")}>
@@ -13,18 +15,16 @@ export default function SideBar() {
         </div>
         <div className={cn("list")}>
           <div className={cn("wrapper")}>
-            <div className={cn("channelName", "item")}>
-              <span className={cn("icon")}>#</span>
-              <span className={cn("")}>랜덤</span>
-            </div>
-            <div className={cn("channelName", "item")}>
-              <span className={cn("icon")}>#</span>
-              <span className={cn("")}>랜덤</span>
-            </div>
-            <div className={cn("channelName", "item")}>
-              <span className={cn("icon")}>#</span>
-              <span className={cn("")}>랜덤</span>
-            </div>
+            {channels.map((channel) => (
+              <Link
+                to={`/channels/${channel.id}`}
+                key={channel.id}
+                className={cn("channelName", "item")}
+              >
+                <span className={cn("icon")}>#</span>
+                <span className={cn("")}>{channel.name}</span>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
