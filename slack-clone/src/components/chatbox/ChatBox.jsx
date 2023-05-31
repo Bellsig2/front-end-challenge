91815140766a54b8dff1cc0c37e7da3ed12186a8
Bellsig2 <75826074+Bellsig2@users.noreply.github.com>
2023-05-31 21:25:47 +0900

    함수 모듈화

--- slack-clone/src/components/chatbox/ChatBox.jsx
+++ slack-clone/src/components/chatbox/ChatBox.jsx
@@ -2,30 +2,11 @@ import { useState } from "react";
 import style from "./ChatBox.module.scss";
 import classnames from "classnames/bind";
 import Message from "../message/Message";
-import UseInput from "../hook/UseInput";
+import UseInput from "../../hook/UseInput";
+import { timestampToDate } from "../../utils/date";
 
+import { editMessage, deleteMessage } from "../../utils/message";
 const cn = classnames.bind(style);
-
-const editMessage = (messages, setMessages, client_msg_id, text, callback) => {
-  const TMP_message = [...messages];
-  for (const message of TMP_message) {
-    if (message.client_msg_id === client_msg_id) {
-      message.text = text;
-      setMessages(TMP_message);
-      callback();
-    }
-  }
-};
-const deleteChannel = (chatLog, log, setChatLog, channelId) => {
-  let chatLogTmp = chatLog;
-
-  for (let i = 0; i < chatLogTmp.length; i++) {
-    if (chatLogTmp[i].channel_id === channelId && chatLogTmp[i].client_msg_id === log.client_msg_id) {
-      chatLogTmp.splice(i, 1);
-    }
-  }
-  setChatLog([...chatLogTmp]);
-};
 
 export default function ChatBox({ log, chatLog, channel, setChatLog }) {
   const [editMode, setEditMode] = useState(false);
@@ -41,15 +22,19 @@ export default function ChatBox({ log, chatLog, channel, setChatLog }) {
           <div className={cn("")}>
             <div className={cn("userWrapper")}>
               <div className={cn("userName")}>{log.user_profile.real_name}</div>
-              <span className={cn("timeStamp")}>오후 3:39</span>
-              <button className={cn("button")} onClick={() => setEditMode(true)}>
+              <span className={cn("timeStamp")}>{timestampToDate(log.ts)}</span>
+              <button
+                className={cn("button")}
+                onClick={() => setEditMode(true)}
+              >
                 수정
               </button>
               <button
                 className={cn("button")}
                 onClick={() => {
-                  deleteChannel(chatLog, log, setChatLog, channel.id);
-                }}>
+                  deleteMessage(chatLog, log, setChatLog, channel.id);
+                }}
+              >
                 삭제
               </button>
             </div>
@@ -58,12 +43,19 @@ export default function ChatBox({ log, chatLog, channel, setChatLog }) {
         ) : (
           <Message
             onSubmit={(e) => {
-              editMessage(chatLog, setChatLog, log.client_msg_id, message, () => {
-                setEditMode(false);
-              });
+              editMessage(
+                chatLog,
+                setChatLog,
+                log.client_msg_id,
+                message,
+                () => {
+                  setEditMode(false);
+                }
+              );
             }}
             onInput={setMessage}
-            message={message}></Message>
+            message={message}
+          ></Message>
         )}
       </div>
     </div>
