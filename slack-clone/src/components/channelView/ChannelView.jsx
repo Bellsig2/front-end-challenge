f790d502f3a43c3d6867b4ad11da1f565e651db7
Bellsig2 <75826074+Bellsig2@users.noreply.github.com>
2023-05-31 21:25:23 +0900

    재귀함수 오류 수정

--- slack-clone/src/components/channelView/ChannelView.jsx
+++ slack-clone/src/components/channelView/ChannelView.jsx
@@ -8,24 +8,28 @@ export default function ChannelView({ channels, chatLog, setChatLog, key }) {
   const viewRef = useRef();
   const [isLoad, setIsLoad] = useState(false);
   let [channel, setChannel] = useState(null);
-  const loading = () => {
-    if (channel === null || !isLoad) {
-      const channelTmp = channels.find((channel) => channel.id === channelID);
-      setChannel(channelTmp);
-      if (!channelTmp) {
-        navigate("/");
-      }
-      setIsLoad(true);
-    } else if (channel !== null && isLoad) {
-      return null;
-    }
-  };
 
   useEffect(() => {
-    loading();
-  }, [channel]);
+    const channelTmp = channels.find((channel) => channel.id === channelID);
+    setChannel(channelTmp);
+    if (!channelTmp) {
+      navigate("/");
+    }
+    setIsLoad(true);
+  }, [channel, channelID, isLoad]);
 
   return (
-    <>{isLoad === true ? <View ref={viewRef} channel={channel} chatLog={chatLog} setChatLog={setChatLog} /> : ""}</>
+    <>
+      {isLoad === true ? (
+        <View
+          ref={viewRef}
+          channel={channel}
+          chatLog={chatLog}
+          setChatLog={setChatLog}
+        />
+      ) : (
+        ""
+      )}
+    </>
   );
 }
